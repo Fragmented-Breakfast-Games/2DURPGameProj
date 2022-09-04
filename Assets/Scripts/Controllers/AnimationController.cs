9665c0598870733e644996b5b022bf2587234943
Charlotte <ayy.charlotte@gmail.com>
2022-09-04 11:53:10 -0400

    Added a very basic player controller
    Signed-off-by: Charlotte <ayy.charlotte@gmail.com>

---
+++ Assets/Scripts/Controllers/AnimationController.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+//using Platformer.Core;
+//using Platformer.Model;
+using UnityEngine;
+
+namespace Controllers
+{
+    public class AnimationController
+    {
+        
+        
+    }
+}
