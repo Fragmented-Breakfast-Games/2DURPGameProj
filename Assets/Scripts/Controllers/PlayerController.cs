f8b297d8840282f339c689712bd744b695343d08
Charlotte <ayy.charlotte@gmail.com>
2022-09-06 19:21:02 -0400

    Double Jump, Camera, Debugging
    Signed-off-by: Charlotte <ayy.charlotte@gmail.com>

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -19,8 +19,8 @@ namespace Controllers
     public class PlayerController : MonoBehaviour
     {
         // Move player in 2D space
-        public float maxSpeed = 5.5f;
-        public float jumpHeight = 15.5f;
+        public float maxSpeed = 6.5f;
+        public float jumpHeight = 18.0f;
         public float gravityScale = 2.5f;
         public Camera mainCamera;
         
@@ -32,12 +32,10 @@ namespace Controllers
         private float jumpBufferCounter;
         private bool isJumping;
         
-        private int jumpCount; //added this to keep track of jumps
-        public int maxJumps = 2; //added this to keep track of jumps
+        private bool canDoubleJump; //bool to check if we can double jump
 
         public bool canThrust = true; // if the player can thrust
         public bool isThrusting; //added this to keep track of thrusting
-        
 
         bool facingRight = true;
         float moveDirection = 0;
@@ -106,8 +104,19 @@ namespace Controllers
             // Jumping
             if (Input.GetKeyDown(KeyCode.Space))
             {
-                Debug.Log("Jumping");
-                this.Jump();
+                
+                if (isGrounded)
+                {
+                    Debug.Log("Jumping");
+                    r2d.velocity = new Vector2(r2d.velocity.x, jumpHeight);
+                    canDoubleJump = true;
+                }
+                else
+                if(canDoubleJump){
+                    Debug.Log("Double Jumping");
+                    canDoubleJump = false;
+                    r2d.velocity = new Vector2(r2d.velocity.x, jumpHeight * 0.80f);
+                }
                 //should be a double jump
             }
             //coyote time and jump buffer time
@@ -134,7 +143,6 @@ namespace Controllers
             {
                 r2d.velocity = new Vector2(r2d.velocity.x, jumpHeight);
                 coyoteTimeCounter = 0f;
-                StartCoroutine(JumpCooldown());
             }
             if (Input.GetKeyUp(KeyCode.Space) && r2d.velocity.y > 0f)
             {
@@ -159,7 +167,12 @@ namespace Controllers
             // Camera follow
             if (mainCamera)
             {
-                mainCamera.transform.position = new Vector3(t.position.x, cameraPos.y, cameraPos.z);
+                //main camera follows player
+                cameraPos.x = t.position.x;
+                cameraPos.y = t.position.y;
+                mainCamera.transform.position = cameraPos;
+                //actually fixed it this time
+                
             }
         }
 
@@ -191,41 +204,17 @@ namespace Controllers
             Debug.DrawLine(groundCheckPos, groundCheckPos - new Vector3(0, colliderRadius, 0), isGrounded ? Color.green : Color.red);
             Debug.DrawLine(groundCheckPos, groundCheckPos - new Vector3(colliderRadius, 0, 0), isGrounded ? Color.green : Color.red);
         }
-        
-        private void Jump()
-        {
-            r2d.velocity = new Vector2(r2d.velocity.x, jumpHeight);
-            if(jumpCount > 0)
-            {
-                gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpHeight), ForceMode2D.Impulse);
-                isGrounded = false;
-                jumpCount = jumpCount - 1;
-            }
-            if(jumpCount== 0)
-            {
-                return;
-            }
-            StartCoroutine(JumpCooldown());
-        }
 
         private void OnCollisionEnter2D(Collision2D collider)
         {
             if(collider.gameObject.CompareTag("Ground"))
             {
-                jumpCount = maxJumps;
                 isGrounded = true;
-                maxSpeed = 5.5f;
+                maxSpeed = 6.5f;
             }
             
         }
-        // cool down coroutine
-        private IEnumerator JumpCooldown()
-        {
-            isJumping = true;
-            yield return new WaitForSeconds(0.5f);
-            isJumping = false;
-        }
-        
+      
         private IEnumerator ThrustCooldown()
         {
             //thrust once then cooldown
@@ -234,10 +223,12 @@ namespace Controllers
                 isThrusting = true;
                 r2d.AddForce(new Vector2(5000f*moveDirection, 0f));
                 canThrust= false;
-                Debug.Log("Thrusting");
                 // blink red for 5 seconds then can thrust again
                 StartCoroutine(BlinkRed());
+                // screen text 
+                Debug.Log("Thrust Cooldown Started");
                 yield return new WaitForSeconds(5.0f);
+                Debug.Log("Thrust Cooldown Ended");
                 canThrust = true;
                 
             }
